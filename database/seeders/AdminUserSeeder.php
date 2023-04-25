490f1c72dae0e1d2394207cb908c488ea3034c3f
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-04-25 14:50:55 -0300

    Campo adicionado na tabela user e seed de admin criada

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123'),
+            'email_verified_at' => now(),
+            'is_admin' => true
+        ]);
+    }
+}
