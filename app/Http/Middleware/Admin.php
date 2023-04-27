5499345fcf6c1bebf1e0519baabb9b5557c695de
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-04-27 12:32:26 -0300

    Middleware para administradores criado

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class Admin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user() && Auth::user()->is_admin == 1) {
+            return $next($request);
+        }
+
+        return response([
+            'message' => 'Você não tem permissão para executar esta ação',
+        ], 403);
+    }
+}
