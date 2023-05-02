7c68f2d775a2693b618e91c54517bf09b3dca674
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-02 15:00:53 -0300

    Criado action para criar o produto e mutation para setar o produto no state

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -19,21 +19,52 @@ export function logout({commit}) {
         })
 }
 
-export function getProducts({commit}, data) {
-    return axiosClient.get('/product')
-        .then((res => {
-            // console.log(res.data);
-        }));
+export function getProducts({commit, state}, {url = null, search = '', per_page, sort_field, sort_direction} = {}) {
+    commit('setProducts', [true]);
+
+    url = url || '/product';
+
+    const params = {
+        per_page: state.products.limit
+    };
+
+    return axiosClient.get(url, {
+        params: {
+            ...params,
+            search, sort_field, sort_direction, per_page
+        }
+    })
+    .then(res => {
+        commit('setProducts', [false, res.data]);
+    })
+    .catch(err => {
+        console.log(err);
+        commit('setProducts', [false]);
+    })
 }
 
 export function getProduct({commit}, idProduct) {
-    return axiosClient.get(`/products/${idProduct}`);
+    return axiosClient.get(`/product/${idProduct}`);
 }
 
 export function deleteProduct({commit}, idProduct) {
-    return axiosClient.delete(`/products/${idProduct}`);
+    return axiosClient.delete(`/product/${idProduct}`);
 }
 
 export function updateProduct({commit}, product) {
-    return axiosClient.put(`/products/${product}`);
+    return axiosClient.put(`/product/${product}`);
+}
+
+export function createProduct({commit}, product) {
+    if (product.image instanceof File) {
+        const form = new FormData();
+        form.append('title', product.title);
+        form.append('image', product.image);
+        form.append('price', product.price);
+        form.append('description', product.description);
+        form.append('published', product.published);
+        product = form;
+    }
+
+    return axiosClient.post('/product', product);
 }
