c9e94ff3b5b377541e3b26dc5ac065c365d54e97
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-08 14:32:05 -0300

    Caminho do controlador da api alterado

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductListResource;
 use App\Http\Resources\ProductResource;
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $perPage = request('perPage', 10);
+        $search = request('search', '');
+        $sortField = request('sortField', 'updated_at');
+        $sortDirection = request('sortDirection', 'asc');
+
+        $query = User::query()
+            ->where('name', 'like', "%{$search}%")
+            ->orderBy($sortField, $sortDirection)
+            ->paginate($perPage);
+
+        return new UserResource($query);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateUserRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = \bcrypt($data['password']);
+
+        $emailUsed = User::where('email', '=', $data['email'])->get();
+
+        if ($emailUsed) {
+            return \response("Este email já existe.");
+        }
+
+        $user = User::create($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
