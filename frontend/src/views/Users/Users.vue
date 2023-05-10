1cd9751d631b7a0a0796e1d27d842f1e76a9217a
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-10 14:58:07 -0300

    Tela principal e tabela de usuario criada

--- frontend/src/views/Users/Users.vue
+++ frontend/src/views/Users/Users.vue
@@ -1,5 +1,38 @@
 <template>
-    <div>
+    <div class="flex items-center justify-between mb-3">
         <h1 class="text-3xl font-semibold">Usuários</h1>
+
+        <button
+            class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            @click="showAddNewModal()"
+        >
+            Add novo usuário
+        </button>
     </div>
+
+    <UsersTable />
 </template>
+
+<script>
+
+import UsersTable from './UsersTable.vue';
+
+export default {
+    components: {
+        UsersTable
+    },
+
+    data() {
+        return {
+            showModelProduct: false
+        }
+    },
+
+    methods: {
+        showAddNewModal() {
+            this.showModelProduct = true;
+        }
+    }
+}
+
+</script>
---
+++ frontend/src/views/Users/UsersTable.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="bg-white p-4 rounded-lg shadow animate-fade-in-down">
+        <div class="flex justify-between border-b-2 pb-3">
+            <div class="flex items-center">
+                <span class="whitespace-nowrap mr-3">Por Página</span>
+
+                <select @change="getUsers(null)" v-model="perPage"
+                    class="appearance-none relative block w-24 px-3 py-2 border border-gray-500 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+
+                <span class="ml-3">Encontrado {{ !users.total ? '0' : users.total }} produtos</span>
+            </div>
+
+            <div>
+                <input type="text" @change="getUsers(null)" v-model="search"
+                    class="appearance-none relative block w-48 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Buscar produto">
+            </div>
+        </div>
+
+        <table class="table-auto w-full">
+            <thead>
+                <TableHeaderCell field="id" :sort-field="sortField" :sort-direction="sortDirection"
+                        @click="sortProducts('id')">
+                        ID
+                    </TableHeaderCell>
+
+                    <TableHeaderCell field="name" :sort-field="sortField" :sort-direction="sortDirection"
+                        @click="sortProducts('name')">
+                        Nome
+                    </TableHeaderCell>
+
+                    <TableHeaderCell field="email" :sort-field="sortField" :sort-direction="sortDirection"
+                        @click="sortProducts('email')">
+                        Email
+                    </TableHeaderCell>
+
+                    <TableHeaderCell field="updated_at" :sort-field="sortField" :sort-direction="sortDirection"
+                        @click="sortProducts('updated_at')">
+                        Última atualização
+                    </TableHeaderCell>
+
+                    <TableHeaderCell field="actions">
+                        Ações
+                    </TableHeaderCell>
+            </thead>
+
+            <tbody v-if="users.loading || !users.data.length">
+                <tr>
+                    <td colspan="6">
+                        <Spinner v-if="users.loading" />
+                        <p v-else class="text-center py-8 text-gray-700">
+                            Não possui usuários cadastrados
+                        </p>
+                    </td>
+                </tr>
+            </tbody>
+
+            <tbody v-else>
+                <tr v-for="(user, index) in users.data" :key="index">
+                    <td class="border-b p-2">{{ user.id }}</td>
+                    <td class="border-b p-2">{{ user.name }}</td>
+                    <td class="border-b p-2">{{ user.email }}</td>
+                    <td class="border-b p-2">{{ user.updated_at }}</td>
+                    <td class="border-b p-2 ">
+                        <Menu as="div" class="relative inline-block text-left">
+                            <div>
+                                <MenuButton
+                                    class="inline-flex items-center  justify-center rounded-full w-10 h-10 bg-black bg-opacity-0 text-sm font-medium text-white hover:bg-opacity-5 focus:bg-opacity-5 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                                    <EllipsisVerticalIcon class="h-5 w-5 text-indigo-500" aria-hidden="true" />
+                                </MenuButton>
+                            </div>
+
+                            <transition enter-active-class="transition duration-100 ease-out"
+                                enter-from-class="transform scale-95 opacity-0"
+                                enter-to-class="transform scale-100 opacity-100"
+                                leave-active-class="transition duration-75 ease-in"
+                                leave-from-class="transform scale-100 opacity-100"
+                                leave-to-class="transform scale-95 opacity-0">
+                                <MenuItems
+                                    class="absolute z-10 right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <div class="px-1 py-1">
+                                        <MenuItem v-slot="{ active }">
+                                        <button :class="[
+                                                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                            ]" @click="editUser(user)">
+                                            <PencilIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400"
+                                                aria-hidden="true" />
+                                            Editar
+                                        </button>
+                                        </MenuItem>
+                                        <MenuItem v-slot="{ active }">
+                                        <button :class="[
+                                                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                            ]" @click="deleteUser(user)">
+                                            <TrashIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400"
+                                                aria-hidden="true" />
+                                            Deletar
+                                        </button>
+                                        </MenuItem>
+                                    </div>
+                                </MenuItems>
+                            </transition>
+                        </Menu>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import store from '../../store';
+import TableHeaderCell from '../../components/core/Table/TableHeaderCell.vue';
+import Spinner from "../../components/core/Spinner.vue";
+import {Menu, MenuItems, MenuButton, MenuItem} from '@headlessui/vue';
+import { EllipsisVerticalIcon, PencilIcon, TrashIcon } from '@heroicons/vue/24/outline';
+
+export default {
+
+    components: {
+        TableHeaderCell,
+        Spinner,
+        Menu,
+        MenuItem,
+        MenuItems,
+        MenuButton,
+        EllipsisVerticalIcon,
+        PencilIcon,
+        TrashIcon
+    },
+
+    data() {
+        return {
+            perPage: '',
+            sortField: 'updated_at',
+            sortDirection: 'desc',
+            search: ''
+        }
+    },
+
+
+    mounted() {
+        this.getUsers();
+    },
+
+    methods: {
+        getUsers(url = null) {
+            store.dispatch('getUsers', {
+                url,
+                search: this.search,
+                per_page: this.perPage,
+                sort_field: this.sortField,
+                sort_direction: this.sortDirection
+            });
+        }
+    },
+
+    computed: {
+        users() {
+            return store.state.users;
+        }
+    }
+}
+</script>
