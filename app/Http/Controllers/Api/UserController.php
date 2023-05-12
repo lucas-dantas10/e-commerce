a11b584ad44421b004f71755a687f5036816333c
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-12 16:58:11 -0300

    Ajuste

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -62,9 +62,11 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return new UserResource($user);
     }
 
     /**
