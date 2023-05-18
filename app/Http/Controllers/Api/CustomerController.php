c0510beacca5c0fefeea4308a2bb5978ec6093a3
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-18 15:00:05 -0300

    Método index de paginação criado

---
+++ app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CustomerListResource;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $perPage = request('perPage', 10);
+        $search = request('search', '');
+        $sortField = request('sort_field', 'created_at');
+        $sortDirection = request('sort_direction', 'desc');
+
+        $query = Customer::query()
+            ->with('user')
+            ->orderBy($sortField, $sortDirection);
+
+        if ($search) {
+            $query
+                ->where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%{$search}%")
+                ->join('users', 'customer.user_id', 'user.id')
+                ->orWhere('users.email', 'like', "%{$search}%")
+                ->orWhere('customer.phone', 'like', "%{$search}%");
+        }
+
+        $paginator = $query->paginate($perPage);
+
+        return CustomerListResource::collection($paginator);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
