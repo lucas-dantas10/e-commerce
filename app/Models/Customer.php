5df4efe6fcdd8aac497bf55674b66046d1f49011
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-18 14:59:15 -0300

    Criado o relacionamento de belongsTo para o user

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use App\Enums\AddressType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $foreignKey = 'user_id';
+
+    protected $fillables = [
+        'first_name', 
+        'last_name', 
+        'phone', 
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'customer_id', $this->foreignKey);
+    }
+
+    private function _getAddresses(): HasOne
+    {
+        return $this->hasOne(CustomerAddress::class, 'customer_id', $this->foreignKey);
+    }
+
+    public function shippingAddresses(): HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::ShippingAddresses->value);
+    }
+
+    public function billingAddresses()
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::BillingAddresses->value);
+    }
 }
