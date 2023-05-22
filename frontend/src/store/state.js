7c6f74e61ba02cc0da6f79eb7dfaf5ba197ad791
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-22 12:43:48 -0300

    Dados de customers criado

--- frontend/src/store/state.js
+++ frontend/src/store/state.js
@@ -24,5 +24,16 @@ export default {
         page: 1,
         limit: null,
         total: null
+    },
+
+    customers: {
+        loading: false,
+        data: [],
+        links: [],
+        from: null,
+        to: null,
+        page: 1,
+        limit: null,
+        total: null
     }
 }
