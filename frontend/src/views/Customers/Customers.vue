7525c73f9370d63f9b5630a5419a0244269e212b
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-22 12:44:38 -0300

    Tela inicial de customer começada

--- frontend/src/views/Customers/Customers.vue
+++ frontend/src/views/Customers/Customers.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
+    <div class="flex items-center justify-between mb-3">
         <h1 class="text-3xl font-semibold">Clientes</h1>
     </div>
+
+    <CustomerTable @edit-customer="edit()"/>
 </template>
+
+<script>
+
+import CustomerTable from './CustomerTable.vue';
+
+export default {
+    components: {
+        CustomerTable
+    },
+    methods: {
+        edit(customer) {
+            // edit with store.dispatch
+        }
+    }
+}
+</script>
