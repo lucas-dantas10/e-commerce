82d6270d276d019d0376389115a229fe61198310
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-23 15:03:00 -0300

    Ajuste no nome do perPage para per_page no indes

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -16,7 +16,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        $perPage = request('perPage', 10);
+        $perPage = request('per_page', 10);
         $search = request('search', '');
         $sortField = request('sort_field', 'created_at');
         $sortDirection = request('sort_direction', 'desc');
