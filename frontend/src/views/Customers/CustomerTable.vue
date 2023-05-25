587c2fbe85de3207b4626ee856b5287642ec2dfa
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-25 11:05:56 -0300

    Componente de table dos customers finalizdo

--- frontend/src/views/Customers/CustomerTable.vue
+++ frontend/src/views/Customers/CustomerTable.vue
@@ -5,7 +5,7 @@
             <div class="flex items-center gap-2">
                 <label for="perPage">Por Página</label>
 
-                <select v-model="perPage"
+                <select v-model="perPage" @change="getCustomers(null)"
                     class="appearance-none relative block w-24 px-3 py-2 border border-gray-500 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
                     <option value="5">5</option>
                     <option value="10">10</option>
@@ -17,7 +17,7 @@
             </div>
 
             <div>
-                <input type="text" placeholder="Procurar Cliente"
+                <input type="text" placeholder="Procurar Cliente" v-model="search" @change="getCustomers(null)"
                     class="py-2 px-4 border border-gray-300 placeholder-gray-500 text-sm rounded-md ">
             </div>
         </div>
@@ -103,7 +103,7 @@
                                     class="absolute z-10 right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                     <div class="px-1 py-1">
                                         <MenuItem v-slot="{ active }">
-                                        <router-link :to="{ name: 'app.customers.view', params: { id: customer.id } }" :class="[
+                                        <router-link :to="{ name: 'app.customers', params: { id: customer.id } }" :class="[
                                             active ? 'bg-indigo-600 text-white' : 'text-gray-900',
                                             'group flex w-full items-center rounded-md px-2 py-2 text-sm',
                                         ]">
@@ -131,6 +131,40 @@
             </tbody>
         </table>
 
+        <div v-if="!customers.loading" class="flex justify-between items-center mt-5">
+                <div v-if="customers.data.length">
+                    Mostrando de {{ customers.from }} de {{ customers.to }}
+                </div>
+
+                <nav 
+                    v-if="customers.total > customers.limit"
+                    class="relative z-0 inline-flex justify-center rounded-md shadow-sm space-x-px"    
+                    aria-label="Pagination"
+                >
+
+                    <a
+                        v-for="(link, index) in customers.links"
+                        :key="index"
+                        :disabled="!link.url"
+                        href="#"
+                        @click="getForPage($event, link)"
+                        class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+                        :class="[
+                            link.active
+                                ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                                : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                            i === 0 ? 'rounded-l-md' : '',
+                            i === customers.links.length - 1 ? 'rounded-r-md' : '',
+                            !link.url ? ' bg-gray-100 text-gray-700': ''
+                            ]"
+                        
+                        v-html="link.label"
+
+                    >
+                    </a>
+
+                </nav>
+        </div>
     </div>
 </template>
 
