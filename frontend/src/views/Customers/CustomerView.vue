a0ea35e3bdc8998ee9ecdd16b4e99731ac66b645
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-29 11:50:37 -0300

    Primeiro parte do componente criado

--- frontend/src/views/Customers/CustomerView.vue
+++ frontend/src/views/Customers/CustomerView.vue
@@ -1,5 +1,43 @@
 <template>
-    <div>
-        <h1>edit</h1>
+    <div v-if="customer.id" class="animate-fade-in-down">
+        <form @submit.prevent="onSubmit()">
+            <div class="bg-white px-4 pt-5 pb-4">
+                <h1 class="text-2xl font-semibold pb-2">{{ title }}</h1>
+                <CustomInput class="mb-2" v-model:modelInput="customer.first_name" label="Primeiro Nome" />
+                <CustomInput class="mb-2" v-model:modelInput="customer.last_name" label="Último Nome" />
+                <CustomInput class="mb-2" v-model:modelInput="customer.email" label="Email" />
+                <CustomInput class="mb-2" v-model:modelInput="customer.phone" label="Telefone" />
+                <CustomInput type="checkbox" class="mb-2" v-model:modelInput="customer.status" label="Ativo" />
+            </div>
+
+        </form>
     </div>
 </template>
+
+<script>
+import store from '../../store';
+import CustomInput from '../../components/core/CustomInput.vue';
+export default {
+    components: {
+        CustomInput
+    },
+
+    data() {
+        return {
+            title: '',
+            customer: '',
+        }
+    },
+
+    mounted() {
+        store.dispatch('getCustomer', this.$route.params.id)
+            .then(({ data }) => {
+                this.title = `Atualizando Cliente "${data.first_name} ${data.last_name}"`;
+                this.customer = data;
+            })
+            .catch(({ response }) => console.log(response));
+    
+    },
+}
+
+</script>
