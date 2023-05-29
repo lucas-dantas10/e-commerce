7b3a117618b66b306e9fec056b3d5b9e9e5dc88a
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-05-29 11:49:56 -0300

    Comentado parte que não esta com relacionamento pronto

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CustomerResource extends JsonResource
 {
+
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
@@ -15,11 +17,11 @@ class CustomerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $shipping = $this->shipping;
-        $billing = $this->billing;
+        // $shipping = $this->shipping;
+        // $billing = $this->billing;
 
         return [
-            'id' => $this->user_id,
+            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
@@ -28,25 +30,25 @@ class CustomerResource extends JsonResource
             'created_at' => (new DateTime($this->created_at))->format('Y-m-d H:i:s'),
             'updated_at' => (new DateTime($this->updated_at))->format('Y-m-d H:i:s'),
 
-            'shippingAddress' => [
-                'id' => $shipping->id,
-                'address1' => $shipping->address1,
-                'address2' => $shipping->address2,
-                'city' => $shipping->city,
-                'state' => $shipping->state,
-                'zipcode' => $shipping->zipcode,
-                'country_code' => $shipping->country->code,
-            ],
+            // 'shippingAddress' => [
+            //     'id' => $shipping->id,
+            //     'address1' => $shipping->address1,
+            //     'address2' => $shipping->address2,
+            //     'city' => $shipping->city,
+            //     'state' => $shipping->state,
+            //     'zipcode' => $shipping->zipcode,
+            //     'country_code' => $shipping->country->code,
+            // ],
 
-            'billingAddress' => [
-                'id' => $billing->id,
-                'address1' => $billing->address1,
-                'address2' => $billing->address2,
-                'city' => $billing->city,
-                'state' => $billing->state,
-                'zipcode' => $billing->zipcode,
-                'country_code' => $billing->country->code,
-            ]
+            // 'billingAddress' => [
+            //     'id' => $billing->id,
+            //     'address1' => $billing->address1,
+            //     'address2' => $billing->address2,
+            //     'city' => $billing->city,
+            //     'state' => $billing->state,
+            //     'zipcode' => $billing->zipcode,
+            //     'country_code' => $billing->country->code,
+            // ]
         ];
     }
 }
