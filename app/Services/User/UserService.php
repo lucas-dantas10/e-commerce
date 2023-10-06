6df3f73ee2e65615fd4d6afaeec8a43fb60df033
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-06 10:51:38 -0300

    Criado User Service

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -5,6 +5,7 @@ namespace App\Services\User;
 use App\Http\Resources\UserResource;
 use App\Repository\UserRepository;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Hash;
 
 class UserService 
 {
@@ -23,5 +24,16 @@ class UserService
         $query = $this->userRepository->filterUsers($search, $sortField, $sortDirection, $perPage);
        
         return UserResource::collection($query);
+    }
+
+    public function createUser(array $data): JsonResource
+    {
+        $data['is_admin'] = true;
+        $data['email_verified_at'] = date('Y-m-d H:i:s');
+        $data['password'] = Hash::make($data['password']);
+        
+        $user = $this->userRepository->create($data);
+
+        return new UserResource($user);
     }
 }
