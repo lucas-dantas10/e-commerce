440bec4946658a130aecaec4bdc35cb83c07462b
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-09 15:32:04 -0300

    Adicionado classe para rodar o seed de countries

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminUserSeeder::class
+            AdminUserSeeder::class,
+            CountrySeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
