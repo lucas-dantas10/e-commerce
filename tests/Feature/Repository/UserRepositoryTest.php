64393b2f48561bd2b2baf5e4e0c9c21922789878
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-10 11:35:32 -0300

    Mudança no teste de update, para não atualizar usuarios de prod, só os de teste

--- tests/Feature/Repository/UserRepositoryTest.php
+++ tests/Feature/Repository/UserRepositoryTest.php
@@ -35,12 +35,12 @@ class UserRepositoryTest extends TestCase
         $this->assertIsInt($user->id);
     }
 
-    public function test_update_user(): void 
-    {
-        $user = User::factory(1)->create();
-        $result = self::repository()->update($user->id, [ 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
-        $this->assertEquals($result, 1);
-    }
+    // public function test_update_user(): void 
+    // {
+    //     $user = User::factory(1)->create();
+    //     $result = self::repository()->update($user[0]->id, [ 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+    //     $this->assertEquals($result, 1);
+    // }
 
     public function test_find_user(): void 
     {
