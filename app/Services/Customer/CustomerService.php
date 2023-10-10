c0c70cd75be361f1eaf94fa47c4c5108dba9c3cd
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-10 11:37:43 -0300

    Implementação de service dos customers, melhorando organização de código

--- app/Services/Customer/CustomerService.php
+++ app/Services/Customer/CustomerService.php
@@ -2,8 +2,15 @@
 
 namespace App\Services\Customer;
 
+use App\Enums\AddressType;
+use App\Http\Requests\CustomerRequest;
 use App\Http\Resources\CustomerListResource;
+use App\Http\Resources\CustomerResource;
+use App\Models\CustomerAddress;
 use App\Repository\CustomerRepository;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CustomerService
 {
@@ -24,6 +31,59 @@ class CustomerService
             $search, $sortField, $sortDirection, $perPage
         );
 
-        return CustomerListResource::collection($paginator);
+        return $paginator;
+    }
+
+    public function showCustomer(int $id)
+    {
+        $customer = $this->customerRepository
+                ->find($id)
+                ->with('user', 'shippingAddresses', 'billingAddresses')
+                ->first();
+
+        return $customer;
+    }
+
+    public function validateUpdateCustomer(CustomerRequest $request, int $id)
+    {
+        $customerData = $request->validated();
+        $customerData['updated_by'] = $request->user()->id;
+        $customerData['status'] = $customerData['status'] ? 1 : 0;
+        $shippingData = $customerData['shippingAddress'];
+        $billingData = $customerData['billingAddress'];
+        $customer = $this->customerRepository->find($id);
+
+        DB::beginTransaction();
+
+        try {
+            $customer->update($customerData);
+
+            if ($customer->shippingAddresses) {
+                $customer->shippingAddresses->update($shippingData);
+            } else {
+                $shippingData['customer_id'] = $customer->user_id;
+                $shippingData['type'] = AddressType::ShippingAddresses->value;
+                CustomerAddress::create($shippingData);
+            }
+    
+            if ($customer->billingAddresses) {
+                $customer->billingAddresses->update($billingData);
+            } else {
+                $billingData['customer_id'] = $customer->user_id;
+                $billingData['type'] = AddressType::BillingAddresses->value;
+                CustomerAddress::create($billingData);
+            }
+        } catch (Exception $err) {
+            DB::rollBack();
+
+            Log::critical(__METHOD__ . ' method does not work. '. $err->getMessage());
+            throw $err;
+        }
+
+        DB::commit();
+
+        return $customer;
+
+        return new CustomerResource($customer);
     }
 }
