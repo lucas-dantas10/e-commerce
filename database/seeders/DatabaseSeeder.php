3fce7b3ab960cc4b07f6f6089c5056f0e4df1631
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-10 11:36:52 -0300

    Adicionado classe de CustomersSeed na seed principal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminUserSeeder::class,
-            CountrySeeder::class
+            CountrySeeder::class,
+            CustomerSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
