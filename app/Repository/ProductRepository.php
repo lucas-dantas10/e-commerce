56ddc948f80c232db95688c98d37cb81243ec9b1
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-11 11:29:49 -0300

    Criado repository de produto

---
+++ app/Repository/ProductRepository.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Repository;
+
+use App\Models\Product;
+
+class ProductRepository extends AbstractRepository
+{
+    protected static $model = Product::class;
+
+    public function filterProducts(
+        string $search, string $sortField, string $sortDirection, int $perPage
+    )
+    {
+        $query = Product::query()
+            ->where('title', 'like', "%{$search}%")
+            ->orderBy($sortField, $sortDirection)
+            ->paginate($perPage);
+
+        return $query;
+    }
+}
