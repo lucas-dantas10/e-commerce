741625489a94fdae80f372c719829122ab4d7f84
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-22 11:17:01 -0300

    Mudança na porta que vai executar o server do frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    server: {
+        port: '5177'
+    },
+    plugins: [vue()],
 })
