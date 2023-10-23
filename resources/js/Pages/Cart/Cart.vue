69908316c7183e3e9a31c208575c1308b44e7865
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-23 10:58:16 -0300

    Criada page de carrinho

---
+++ resources/js/Pages/Cart/Cart.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Carrinho</h2>
+        </template>
+    </AuthenticatedLayout>
+</template>
