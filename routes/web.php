bb860652975663fec17ffcb4d38d00cefb4f7df2
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-24 15:41:11 -0300

    Mudanã da rota principal e adicionado middleware guestOrVerified

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', [ProductController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['guestOrVerified'])->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('dashboard');;
+}); 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
