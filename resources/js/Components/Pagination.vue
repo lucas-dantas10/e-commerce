63065fc089de0e7cded749d9d29015e191142830
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-24 15:39:12 -0300

    Criado paginate da página Home

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
+
+const props = defineProps({
+    pagination: {
+        type: Object,
+        required: true,
+    },
+});
+
+onMounted(() => {
+    console.log(props.pagination);
+})
+</script>
+
+<template>
+    <div class="w-full p-6 flex justify-between items-center">
+        <div>
+            <p class="text-gray-500">
+                Mostrando {{ pagination.to }} de {{ pagination.total }}
+            </p>
+        </div>
+
+        <nav class="relative flex justify-center">
+            <template v-for="link in pagination.links" :key="link.label">
+                <Link 
+                    preserve-scroll
+                    :href="link.url ?? ''" 
+                    v-html="link.label" 
+                    class="flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600"
+                    :class="{'bg-gray-200': link.active, '!text-gray-400': !link.url}"
+                />
+            </template>
+        </nav>
+    </div>
+</template>
