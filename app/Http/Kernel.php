02d9fba43d92cac32e2543cee34fbfdcb95df7b5
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-25 11:31:44 -0300

    Adicionando middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\Admin;
+use App\Http\Middleware\GuestOrVerified;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -67,6 +68,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
+
+        'guestOrVerified' => GuestOrVerified::class,
         'admin' => Admin::class
     ];
 }
