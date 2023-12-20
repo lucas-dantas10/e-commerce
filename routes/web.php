56ef6883a3ea9529e58696463358b80ddacd8efe
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-20 14:38:57 -0300

    :sparkles: feat: create routeresource of cart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -15,9 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/carrinho', function () {
-        return Inertia::render('Cart/Cart');
-    })->name('cart');
+    Route::resource('/carrinho', CartController::class);
 });
 
 require __DIR__ . '/auth.php';
