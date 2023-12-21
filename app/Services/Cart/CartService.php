cd79dbc1310a972f1b3aac811968525dcc9f6b26
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-21 15:16:36 -0300

    :sparkles: feat: create service of cart_items

---
+++ app/Services/Cart/CartService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services\Cart;
+
+use App\Models\CartItem;
+use App\Models\Product;
+use App\Repository\CartItemsRepository;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Number;
+
+class CartService
+{
+    public function __construct(
+        protected CartItemsRepository $cartRepository,
+    ) {
+    }
+
+    public function getCartItemsWithProducts()
+    {
+        $cartItems = $this->cartRepository->getCartItems();
+
+        $productsIds = Arr::pluck($cartItems, 'product_id');
+        $productsOfCart = Product::query()->whereIn('id', $productsIds)->get();
+        $cartItems = Arr::keyBy($cartItems, 'product_id');
+
+        $total = 0;
+        foreach ($productsOfCart as $product) {
+            $total += $product->price * $cartItems[$product->id]['quantity'];
+        }
+        $totalFormated = str_replace('R$', 'R$ ', Number::currency($total, 'BRL'));
+
+        return [
+            $cartItems,
+            $productsOfCart,
+            $totalFormated,
+        ];
+    }
+
+    public function updateQuantityOfCart($productId, $dataValidated)
+    {
+        $cartItem = CartItem::where('product_id', $productId)
+            ->where('user_id', auth()->user()->id);
+
+        $updated = $cartItem->update(['quantity' => $dataValidated['quantity']]);
+
+        if ($updated) {
+            return \redirect()->back();
+        }
+
+        return;
+    }
+}
