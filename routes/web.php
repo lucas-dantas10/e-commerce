cbf828e0145ed7d79053299d0967e1979348ab03
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-21 15:18:07 -0300

    :recycle: refactor: remove importations not important

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware(['guestOrVerified'])->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('dashboard');;
