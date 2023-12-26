87aec6b9c198d3417a96a6d2ebf9678ae5ec6839
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-26 17:41:03 -0300

    :lipstick: style: estilização da lista de toast finalizada

---
+++ resources/js/Components/ToastList.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ToastListItem from "@/Components/ToastListItem.vue";
+import { router, usePage } from "@inertiajs/vue3";
+import { onUnmounted } from "vue";
+import toast from "@/Stores/toast";
+
+const page = usePage();
+
+let removeFinishEventListener = router.on("finish", () => {
+    if (page.props.toast) {
+        toast.add({
+            message: page.props.toast,
+        });
+    }
+});
+
+onUnmounted(() => removeFinishEventListener());
+
+function remove(index) {
+    toast.remove(index);
+}
+</script>
+
+<template>
+    <TransitionGroup
+        tag="div"
+        enter-from-class="translate-x-full opacity-0"
+        enter-active-class="duration-500"
+        leave-active-class="duration-500"
+        leave-to-class="translate-x-full opacity-0"
+        class="fixed top-4 right-4 z-50 space-y-4 w-full max-w-xs"
+    >
+        <ToastListItem
+            v-for="(item, index) in toast.items"
+            :key="item.key"
+            :message="item.message"
+            @remove="remove(index)"
+        />
+    </TransitionGroup>
+</template>
