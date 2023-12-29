82b53f24ba27d9da7eef3135f17ee8664a93b142
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-29 16:29:19 -0300

    :construction: feat: criando estilização da página de visualização do produto

---
+++ resources/js/Pages/Product/ProductView.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    product: {
+        type: Array,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <section class="w-full h-full flex justify-between">
+        <div class="w-full h-[240px] sm:h-[400px] flex items-center justify-center">
+            <img :src="product.image" :alt="product.title" class="w-auto h-auto max-h-full mx-auto">
+        </div>
+        <div>
+            <p></p>
+            <p></p>
+            <div>
+                <span></span>
+                <input type="number">
+            </div>
+        </div>
+    </section>
+</template>
