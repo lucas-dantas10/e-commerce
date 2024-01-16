819b7df4368fa615ca815f87fdf9695585d74f6f
Lucas <lucas.dantas.nogueira@gmail.com>
2024-01-16 18:16:23 -0300

    :sparkles: feat: helper para ajuda de busca e querys do cart

---
+++ app/Helpers/Cart.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace App\Helpers;
+
+use App\Models\CartItem;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
+
+class Cart 
+{
+    public static function getCartItems()
+    {
+        $currentUser = request()->user();
+
+        $cartItems = CartItem::where('user_id', $currentUser->id)
+            ->get()
+            ->map(
+                fn($item) => ['product_id' => $item->product_id, 'quantity' => $item->quantity]
+            );
+
+        return $cartItems;
+    }
+
+    public static function getProductsAndCartItems(): array | Collection
+    {
+        $cartItems = self::getCartItems();
+
+        $productsIds = Arr::pluck($cartItems, 'product_id');
+        $productsOfCart = Product::query()->whereIn('id', $productsIds)->get();
+        $cartItems = Arr::keyBy($cartItems, 'product_id');
+
+        return [$productsOfCart, $cartItems];
+    }
+}
