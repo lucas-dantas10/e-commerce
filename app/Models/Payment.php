952120254add198f6039521e8890cbf4cc8db9eb
Lucas <lucas.dantas.nogueira@gmail.com>
2024-01-17 14:37:05 -0300

    :sparkles: feat: adicionado os fillables

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'amount', 
+        'status',  
+        'type',
+        'created_by',
+        'updated_by', 
+    ];
 }
