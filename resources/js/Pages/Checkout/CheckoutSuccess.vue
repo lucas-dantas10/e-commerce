07294e2c20be0fb199d03ba16391e7e60216b5c9
Lucas <lucas.dantas.nogueira@gmail.com>
2024-01-18 18:16:39 -0300

    :sparkles: feat: adicionada 2 telas, uma para sucesso do pagamento e outra para a falha do pagamento

---
+++ resources/js/Pages/Checkout/CheckoutSuccess.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <template #header>
+            <div class="p-6 mt-4 rounded-md bg-emerald-500 texto-white">
+                <p class="text-gray-50 font-bold">Sucesso!! Pagamento realizado com sucesso.</p>
+
+                <div class="flex items-center">
+                    <a :href="route('dashboard')" class="mt-6 rounded-lg bg-gray-50 px-2 py-1">
+                        Voltar para home
+                    </a>
+                </div>
+            </div>
+        </template>
+    </AuthenticatedLayout>
+</template>
