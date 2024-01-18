f94d0b17165357802833861e32fe25dc84143ebe
Lucas <lucas.dantas.nogueira@gmail.com>
2024-01-18 18:19:48 -0300

    :sparkles: feat: adicionada rota de failure no checkout do produto

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('/carrinho', CartController::class);
     Route::post('/checkout', [CheckoutController::class, 'checkout'])->name('cart.checkout');
     Route::get('/checkout-success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('/checkout-failed', [CheckoutController::class, 'failed'])->name('checkout.failed');
     Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 });
 
