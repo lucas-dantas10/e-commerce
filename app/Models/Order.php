15d3dc4e174bb1db5022ce5df9b256272a38322e
Lucas <lucas.dantas.nogueira@gmail.com>
2024-01-18 18:20:27 -0300

    :sparkles: feat: adicionado método de 'payment' para relacionamento com a table 'payments'

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -16,6 +16,11 @@ class Order extends Model
         'updated_by',
     ];
 
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+
     public function user() 
     {
         return $this->belongsTo(User::class, 'created_by');
