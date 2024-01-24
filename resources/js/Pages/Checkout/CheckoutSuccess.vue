265fe010b05c1cf0f021a8e820157687cbfbf1cf
Lucas <lucas.dantas.nogueira@gmail.com>
2024-01-24 14:08:07 -0300

    feat: add prop de customer, para pegar informação do cliente que fez o pagamento e mudança na mensagem de sucesso

--- resources/js/Pages/Checkout/CheckoutSuccess.vue
+++ resources/js/Pages/Checkout/CheckoutSuccess.vue
@@ -1,12 +1,19 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { onMounted } from "vue";
+
+const props = defineProps({
+    customer: Object
+});
+
+onMounted(() => console.log(props.customer))
 </script>
 
 <template>
     <AuthenticatedLayout>
         <template #header>
             <div class="p-6 mt-4 rounded-md bg-emerald-500 texto-white">
-                <p class="text-gray-50 font-bold">Sucesso!! Pagamento realizado com sucesso.</p>
+                <p class="text-gray-50 font-bold">Sucesso!! {{ props.customer.name }}, Seu pedido foi completado!</p>
 
                 <div class="flex items-center">
                     <a :href="route('dashboard')" class="mt-6 rounded-lg bg-gray-50 px-2 py-1">
