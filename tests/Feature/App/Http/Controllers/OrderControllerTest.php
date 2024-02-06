99980d02854d8d538928e78919d37d833905b705
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2024-02-06 16:36:50 -0300

    test: teste para confirmar quantidade de pedidos do usuario logado

--- tests/Feature/App/Http/Controllers/OrderControllerTest.php
+++ tests/Feature/App/Http/Controllers/OrderControllerTest.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Order;
 use Inertia\Testing\AssertableInertia;
 
 uses()->group('orders');
 
-it('return from the orders page has the orders property', function () {
+it('page orders has the orders property', function () {
     $response = $this->get('/orders');
 
     $response->assertInertia(fn (AssertableInertia $page) => $page
@@ -13,3 +14,26 @@ it('return from the orders page has the orders property', function () {
     );
     $response->assertStatus(200);
 });
+
+it('number of orders from the logged in user is correct', function () {
+    $response = $this->get('/orders');
+
+    $orders = Order::query()
+            ->where('created_by', auth()->id())
+            ->count();
+
+    $response->assertInertia(fn (AssertableInertia $page) => $page
+        ->component('Orders/Orders')
+        ->has('orders', $orders)
+    );
+    $response->assertStatus(200);
+});
+
+// it('checks if the subtotal is correct', function () {
+//     $response = $this->get('/orders');
+
+//     $orders = Order::query()
+//             ->where('created_by', auth()->id())
+//             ->count();
+//     dd($orders);
+// });
