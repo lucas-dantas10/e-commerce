fd2d742a639611b816bc1d0300c3d26c38b537b5
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2024-02-08 12:32:23 -0300

    feat: add resource para retorno de orders na tela de 'Meus Pedidos'

---
+++ app/Http/Resources/OrderItemResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Number;
+
+class OrderItemResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+           'id' => $this->id, 
+           'date' => (new DateTime($this->created_at))->format('Y-m-d H:i:s'),
+           'status' => $this->status,
+           'subtotal' => Number::currency($this->total_price, 'BRL'),
+           'quantity_items' => 19
+        ];
+    }
+}
