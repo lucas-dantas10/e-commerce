589b026534f4123f1e5b9469f701710472733ea0
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2024-02-08 14:46:00 -0300

    feat: add relação com OrderItems

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -24,5 +25,10 @@ class Order extends Model
     public function user() 
     {
         return $this->belongsTo(User::class, 'created_by');
+    }
+    
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
     }
 }
